69552d070b6fbc0b8a7b615cd8ad3223f1703a72
robus <robusgauli@gmail.com>
2017-12-07 13:08:07 +0545

    navigation start

---
+++ src/auth/screens/login.screen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+export class LoginScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Hi from the login screen
+        </Text>
+      </View>
+    );
+  }
+}
