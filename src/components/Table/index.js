19b75485a40ce75724fe2fefe6ce1d7d67d32653
shivashp <shivapandey04@gmail.com>
2017-12-07 13:32:34 +0545

    Global Config

---
+++ src/components/Table/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {
+  Text, View,
+} from 'react-native';
+
+class Table extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Table Section</Text>
+      </View>
+    );
+  }
+}
+
+export default Table;
